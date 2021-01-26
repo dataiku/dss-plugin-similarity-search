f002b88bdc071849dbda81a20d5d69eab364f2c8
alexcombessie <alex.combessie@gmail.com>
2021-01-26 12:26:52 +0100

    Rename vector to array everywhere

--- custom-recipes/similarity-search-index/recipe.py
+++ custom-recipes/similarity-search-index/recipe.py
@@ -12,24 +12,24 @@ from dku_io_utils import save_array_to_folder
 # Load parameters
 params = load_indexing_recipe_params()
 
-# Load data into vector format for indexing
+# Load data into array format for indexing
 columns = [params["unique_id_column"]] + params["feature_columns"]
 input_df = params["input_dataset"].get_dataframe(columns=columns, infer_with_pandas=False)
 data_loader = DataLoader(params["unique_id_column"], params["feature_columns"])
-(vector_ids, vectors) = data_loader.convert_df_to_vectors(input_df)
+(array_ids, arrays) = data_loader.convert_df_to_array(input_df)
 
 # Build index and save index file to output folder
-nearest_neighbor = NearestNeighborSearch(num_dimensions=vectors.shape[1], **params)
+nearest_neighbor = NearestNeighborSearch(num_dimensions=arrays.shape[1], **params)
 with NamedTemporaryFile() as tmp:
-    nearest_neighbor.build_save_index(vectors=vectors, index_path=tmp.name)
+    nearest_neighbor.build_save_index(arrays=arrays, index_path=tmp.name)
     index_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.INDEX_FILE_NAME)
     params["index_folder"].upload_stream(index_file_path, tmp)
 
-# Save vector data and indexing config to guarantee reproducibility
-vector_ids_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.VECTOR_IDS_FILE_NAME)
-vectors_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.VECTORS_FILE_NAME)
+# Save arrays and indexing config to guarantee reproducibility
+array_ids_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.ARRAY_IDS_FILE_NAME)
+arrays_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.ARRAYS_FILE_NAME)
 config_file_path = os.path.join(params["folder_partition_root"], nearest_neighbor.CONFIG_FILE_NAME)
-save_array_to_folder(array=vector_ids, path=vector_ids_file_path, folder=params["index_folder"])
-save_array_to_folder(array=vectors, path=vectors_file_path, folder=params["index_folder"])
+save_array_to_folder(array=array_ids, path=array_ids_file_path, folder=params["index_folder"])
+save_array_to_folder(array=arrays, path=arrays_file_path, folder=params["index_folder"])
 config = {**nearest_neighbor.get_config(), **{k: v for k, v in params.items() if k in {"feature_columns", "expert"}}}
 params["index_folder"].write_json(config_file_path, config)
--- python-lib/data_loader.py
+++ python-lib/data_loader.py
@@ -11,21 +11,21 @@ import numpy as np
 
 
 class DataLoader:
-    """Data loading class to convert numeric/vector data from pandas DataFrames into numpy.arrays"""
+    """Data loading class to convert numeric/array data from pandas DataFrames into numpy.arrays"""
 
-    MAX_VECTOR_LENGTH = 2 ** 16  # hardcoded limit to keep array size under 65 536
+    MAX_ARRAY_LENGTH = 2 ** 16  # hardcoded limit to keep array size under 65536
 
     def __init__(self, unique_id_column: AnyStr, feature_columns: List[AnyStr]):
         self.unique_id_column = unique_id_column
         self.feature_columns = feature_columns
 
     @staticmethod
-    def load_vector_from_string(string: AnyStr) -> np.array:
-        """Attempt to load a stringified vector into a numpy.array"""
+    def load_array_from_string(string: AnyStr) -> np.array:
+        """Attempt to load a stringified list into a numpy.array"""
         parsed = json.loads(string)
         if isinstance(parsed, list):
-            vector = np.array(parsed).astype(np.float32)
-            return vector
+            array = np.array(parsed).astype(np.float32)
+            return array
         else:
             raise ValueError(f"string '{string}' is not a list")
 
@@ -39,48 +39,50 @@ class DataLoader:
             if df[column].isnull().values.any():
                 raise ValueError(f"Empty values in column '{column}'")
 
-    def convert_df_to_vectors(self, df: pd.DataFrame, verbose: bool = True) -> Tuple[np.array, np.array]:
-        """Convert a DataFrame into the vector format required by Similarity Search algorithms"""
+    def convert_df_to_array(self, df: pd.DataFrame, verbose: bool = True) -> Tuple[np.array, np.array]:
+        """Convert a DataFrame into the array format required by Similarity Search algorithms"""
         start = perf_counter()
         self._validate_df(df)
         if verbose:
             logging.info(
                 f"Loading dataframe of {len(df.index)} rows and "
-                + f"{len(self.feature_columns)} column(s) into vector format..."
+                + f"{len(self.feature_columns)} column(s) into array format..."
             )
-        vector_ids = df[self.unique_id_column].values
-        vectors_length = self._count_vector_length(df)
-        vectors = self._load_vectors_from_df(df, vectors_length)
+        array_ids = df[self.unique_id_column].values
+        array_length = self._count_array_length(df)
+        arrays = self._load_arrays_from_df(df, array_length)
         if verbose:
             logging.info(
-                f"Loading dataframe into vector format: array of dimensions {vectors.shape} "
+                f"Loading dataframe into array format: dimensions {arrays.shape} "
                 + f"loaded in {perf_counter() - start:.2f} seconds.",
             )
-        return (vector_ids, vectors)
+        return (array_ids, arrays)
 
-    def _load_vectors_from_df(self, df: pd.DataFrame, vector_length: int) -> np.array:
-        vectors = np.empty(shape=(len(df.index), vector_length))  # pre-allocate empty array of fixed size
+    def _load_arrays_from_df(self, df: pd.DataFrame, array_length: int) -> np.array:
+        """Concatenate numeric/array columns of DataFrame into a single numpy.array"""
+        arrays = np.empty(shape=(len(df.index), array_length))  # pre-allocate empty array of fixed size
         i = 0
         for column in self.feature_columns:
-            column_is_vector = df[column].dtype == "object" and df[column].str.startswith("[").all()
-            if column_is_vector:
+            column_is_array = df[column].dtype == "object" and df[column].str.startswith("[").all()
+            if column_is_array:
                 try:
-                    column_array = np.stack(df[column].apply(self.load_vector_from_string), axis=0)
-                    vectors[:, i : (i + column_array.shape[1])] = column_array  # noqa
+                    column_array = np.stack(df[column].apply(self.load_array_from_string), axis=0)
+                    arrays[:, i : (i + column_array.shape[1])] = column_array  # noqa
                     i += column_array.shape[1]
                 except ValueError as e:
-                    raise ValueError(f"Invalid vector data in column '{column}': {e}")
+                    raise ValueError(f"Invalid array data in column '{column}': {e}")
             else:
                 try:
-                    vectors[:, i] = df[column].values.astype(np.float32)
+                    arrays[:, i] = df[column].values.astype(np.float32)
                     i += 1
                 except ValueError as e:
                     raise ValueError(f"Invalid numeric data in column '{column}': {e}")
-        vectors = np.ascontiguousarray(vectors[:, :i], dtype=np.float32)
-        return vectors
+        arrays = np.ascontiguousarray(arrays[:, :i], dtype=np.float32)
+        return arrays
 
-    def _count_vector_length(self, df: pd.DataFrame) -> int:
-        vectors = self._load_vectors_from_df(df.head(1), self.MAX_VECTOR_LENGTH)
-        vectors_length = vectors.shape[1]
-        logging.info(f"Concatenated vector length: {vectors_length}")
-        return vectors_length
+    def _count_array_length(self, df: pd.DataFrame) -> int:
+        """Compute the concatenated array length of a DataFrame with multiple numeric/array columns"""
+        first_array = self._load_arrays_from_df(df.head(1), self.MAX_ARRAY_LENGTH)
+        array_length = first_array.shape[1]
+        logging.info(f"Concatenated array length: {array_length}")
+        return array_length
